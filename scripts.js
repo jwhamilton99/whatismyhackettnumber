8a7bd411e77993f2157d236a88d938a147a9a1d2
Justin Hamilton <jwhamilton99@gmail.com>
2020-04-29 16:11:16 -0400

    div by 0 fixes

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ function computeBase() {
 	var nDesktops = parseInt(document.getElementById("numDesktops").value);
 	var nNotebooks = parseInt(document.getElementById("numNotebooks").value);
 	var niPads = parseInt(document.getElementById("numiPads").value);
-	if(isNaN(nPeople) || isNaN(nDesktops) || isNaN(nNotebooks) || isNaN(niPads)) {
+	if(isNaN(nPeople) || isNaN(nDesktops) || isNaN(nNotebooks) || isNaN(niPads) || nPeople == 0) {
 		return "Please enter your data.";
 	} else {
 		return "Your Hackett Number is: <b>"+(Math.round(((nDesktops+nNotebooks+niPads)/nPeople)*10)/10)+"</b>";
@@ -24,7 +24,7 @@ function computeExtended() {
 	var nNewtons = parseInt(document.getElementById("numNewtons").value);
 	var niPhones = parseInt(document.getElementById("numiPhones").value);
 	var niPods = parseInt(document.getElementById("numiPods").value);
-	if(isNaN(nPeople) || isNaN(nDesktops) || isNaN(nNotebooks) || isNaN(niPads) || isNaN(nNewtons) || isNaN(niPhones) || isNaN(niPods)) {
+	if(isNaN(nPeople) || isNaN(nDesktops) || isNaN(nNotebooks) || isNaN(niPads) || isNaN(nNewtons) || isNaN(niPhones) || isNaN(niPods) || nPeople == 0) {
 		return "Please enter your data.";
 	} else {
 		return "Your extended Hackett Number is: <b>"+(Math.round(((nDesktops+nNotebooks+niPads+nNewtons+niPhones+niPods)/nPeople)*10)/10)+"</b>";
